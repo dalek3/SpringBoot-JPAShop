11006a2eddaa1d52dc4ea819ac1dbdc3ecdf1e9d
dalek3 <lina1318@gmail.com>
2021-10-04 19:28:13 +0900

    API 개발

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -51,4 +51,15 @@ public class MemberService {
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    /**
+     * 회원 수정
+     */
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id).get();
+        member.setName(name);
+
+    }
 }
